28cbc028413de3c4c1d256b11b50a32a50cedee6
Maurice Künicke <m.kuenicke1995@gmail.com>
2021-05-18 22:01:15 +0200

    Implement basic API connection

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,38 +1,74 @@
-import { useState } from 'react'
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from 'react';
 
-function Create() {
-    const [submitting, setSubmitting] = useState(false);
-    const handleSubmit = event => {
-        event.preventDefault();
-        setSubmitting(true);
+class Create extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { Submitting: false, res: false, message_id: null };
 
-        setTimeout(() => {
-            setSubmitting(false);
-        }, 3000)
     }
-    return (
-        <div className="create-form">
-            <form onSubmit={handleSubmit}>
-                <fieldset>
-                    <label>
-                        <p>Your Message</p>
-                        <textarea />
-                    </label>
-                    <label>
-                        <p>Your Passphrase</p>
-                        <input />
-                    </label>
-                </fieldset>
-                <button type="submit">Submit</button>
-                {submitting &&
-                    <p>Creating message....</p>
-                }
-            </form>
-        </div>
-    )
+
+    SubmitHandler = async event => {
+        event.preventDefault();
+        try {
+            this.setState({ Submitting: true });
+            await fetch(process.env.REACT_APP_APICREATEURL, {
+                method: "post",
+                headers: {
+                    "Accept": "application/json",
+                    "Content-type": "application/json",
+                },
+                body: JSON.stringify({
+                    "message": this.state.message,
+                    "passphrase": this.state.passphrase
+                })
+            }).then(response => response.json()).then(data => this.setState({ message_id: data.id }));
+            this.setState({ Submitting: false });
+            this.setState({ res: true });
+            document.getElementById("create-message-form").reset();
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
+    ChangeHandler = (event) => {
+        let key = event.target.name;
+        let val = event.target.value;
+        this.setState({ [key]: val });
+    }
+    render() {
+        return (
+            <div className="create-form">
+                <form id="create-message-form" onSubmit={this.SubmitHandler}>
+                    <p>Message</p>
+                    <textarea
+                        rows="4" cols="50"
+                        type='text'
+                        name="message"
+                        onChange={this.ChangeHandler}
+                    />
+                    <p>Passphrase</p>
+                    <input
+                        type='text'
+                        name="passphrase"
+                        onChange={this.ChangeHandler}
+                    />
+                    <p></p>
+                    <input
+                        type='submit'
+                    />
+                    {this.state.Submitting &&
+                        <p>Creating message....</p>
+                    }
+                    {this.state.res &&
+                        <p>Copy this message ID: <br></br> {this.state.message_id}</p>
+                    }
+                </form>
+            </div>
+        );
+    }
 }
+
 
 export default Create
 
--- src/components/Receive.js
+++ src/components/Receive.js
@@ -1,11 +1,77 @@
 import React from 'react'
 
-function Receive() {
-    return (
-        <div>
+class Receive extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { Submitting: false, res: false, message: null };
 
-        </div>
-    )
+    }
+
+    SubmitHandler = async event => {
+        event.preventDefault();
+        this.setState({ Submitting: true });
+        const url = process.env.REACT_APP_APICREATEURL + "/" + this.state.secret_id
+        await fetch(url, {
+            method: "post",
+            headers: {
+                "Accept": "application/json",
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify({
+                "passphrase": this.state.passphrase
+            })
+        }).then(response => response.json()).then(data => this.setState({ data: data }));
+        const status_code = this.state.data.status_code
+        if (status_code === 404) {
+            this.setState({ message: this.state.data.detail });
+        }
+        else {
+            console.log(this.state.data)
+            this.setState({ message: this.state.data.message });
+        }
+        this.setState({ Submitting: false });
+        this.setState({ res: true });
+        document.getElementById("create-message-form").reset();
+
+    }
+
+    ChangeHandler = (event) => {
+        let key = event.target.name;
+        let val = event.target.value;
+        this.setState({ [key]: val });
+    }
+    render() {
+        return (
+            <div className="create-form">
+                <form id="create-message-form" onSubmit={this.SubmitHandler}>
+                    <p>Secret ID</p>
+                    <input
+                        rows="4" cols="50"
+                        type='text'
+                        name="secret_id"
+                        onChange={this.ChangeHandler}
+                    />
+                    <p>Passphrase</p>
+                    <input
+                        type='text'
+                        name="passphrase"
+                        onChange={this.ChangeHandler}
+                    />
+                    <p></p>
+                    <input
+                        type='submit'
+                    />
+                    {this.state.Submitting &&
+                        <p>Creating message....</p>
+                    }
+                    {this.state.res &&
+                        <p>Your message: <br></br> {this.state.message}</p>
+                    }
+                </form>
+            </div>
+        );
+    }
 }
+
 
 export default Receive
